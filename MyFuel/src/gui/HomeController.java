a6697d1b6d21293cb652cd9ce9575a27f8227d98
abohamad1996 <abohamad1996@users.noreply.github.com>
2020-05-27 01:15:58 +0300

    Login Works!

--- MyFuel/src/gui/HomeController.java
+++ MyFuel/src/gui/HomeController.java
@@ -1,12 +1,14 @@
 package gui;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Application;
+
+
 import Entity.User;
-import client.Func;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,37 +19,12 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+
 public class HomeController implements Initializable{
-	   @FXML
-	    private Button btnAbout;
-
-	    @FXML
-	    private Button btnHome;
-
-	    @FXML
-	    private Button btnProfile;
-
-	    @FXML
-	    private AnchorPane btnOrder;
-
-	    @FXML
-	    private Button btnRegister;
-
-	    @FXML
-	    private Button btnOrderHeating;
-
-	    @FXML
-	    private Button btnPurchase;
-
-	    @FXML
-	    private Label labelDir;
+	
 	public static HomeController s;
 	public static Stage primaryStage;
-	private static User inspector;
-
-
 	public void start(User inspector) {
-		this.inspector = inspector;
 		s = this;
 		primaryStage = LoginController.primaryStage;
 		Platform.runLater(new Runnable() {
@@ -55,9 +32,9 @@ public class HomeController implements Initializable{
 			public void run() {
 				try {
 					Parent root;
-					root = FXMLLoader.load(getClass().getResource("/gui/Login.fxml"));
+					root = FXMLLoader.load(getClass().getResource("/gui/Home.fxml"));
 					Scene scene = new Scene(root);
-					scene.getStylesheets().add(getClass().getResource("/gui/css2.css").toExternalForm());
+				//	scene.getStylesheets().add(getClass().getResource("/gui/css2.css").toExternalForm());
 					primaryStage.setScene(scene);
 					primaryStage.setResizable(false);
 					primaryStage.setTitle("Home");
@@ -69,7 +46,6 @@ public class HomeController implements Initializable{
 
 		});
 	}
-
 	public Stage getPrimaryStage() {
 		return primaryStage;
 	}
@@ -77,7 +53,6 @@ public class HomeController implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
 	}
 }
